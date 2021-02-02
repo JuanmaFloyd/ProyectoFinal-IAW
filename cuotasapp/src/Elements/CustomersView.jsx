85b6c4f078e1315424b5389ca868157f511bbc28
Juan Martin Giron <jgiron@hexacta.com>
2021-02-02 11:19:31 -0300

    barra para filtrar clientes

--- cuotasapp/src/Elements/CustomersView.jsx
+++ cuotasapp/src/Elements/CustomersView.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import { Container, Grid, makeStyles, TextField, Typography } from "@material-ui/core";
 import { CustomerCard } from './CustomerCard';
 import { AddButton } from './AddButton';
 import { Spinner } from './Spinner';
@@ -18,11 +18,13 @@ const useStyles = makeStyles(theme => ({
 
 export const CustomersView = () => {
     const [customers, setCustomers] = useState("");
+    const [allCustomers, setAllCustomers] = useState("");
     const classes = useStyles();
 
     useEffect(() => {
         axios.get('http://localhost:5001/customers', {"headers": {"token": sessionStorage.getItem("token")}})
             .then((response) => {
+                setAllCustomers(response.data);
                 setCustomers(response.data);
             })
             .catch((error) => {
@@ -30,11 +32,17 @@ export const CustomersView = () => {
             })
     }, [setCustomers])
 
+    const handleText = (e) => {
+        const filter = allCustomers.filter(customer => customer.name.includes(e.target.value))
+        setCustomers(filter);
+    }
+
     return (
         customers ? (
         <Container className={classes.grid} maxWidth="md">
+            <TextField label="filtrar" margin="normal" variant="outlined" onChange={handleText} />
             <Grid container spacing={4}>
-            { customers.length > 0 ? (
+            { allCustomers.length > 0 ? (
                 customers.map((customer) => 
                     <Grid item xs={12} sm={6} md={4} key={customer._id}>
                         <CustomerCard 
